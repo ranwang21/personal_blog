a646a7d065a4f5f0002a992f218962c6a1bdd794
ranwang21 <wang.ran.henri@gmail.com>
2019-10-22 23:17:12 +0800

    issue: css does not render on /detail/{id}

--- src/DAL/DAL.php
+++ src/DAL/DAL.php
@@ -1,5 +1,5 @@
 <?php
-
+require '../src/DBHelper/connection.php';
 
 class DAL{
     /**
--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,21 @@
 <?php
-// Routes
+require '../src/models/Post.php';
+require '../src/models/Comment.php';
+require '../src/DAL/DAL.php';
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
+$app->get('/', function ($request, $response, $args) {
+    // get all posts from database
+    $posts = DAL::get_all_post();
+    // insert posts into args
+    $args['posts'] = $posts;
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/detail/{id}', function($request, $response, $args){
+    // get the post by id
+    $post = DAL::get_one_post_by_id($args['id']);
+    // insert the post found into args array
+    $args['post'] = $post;
+    return $this->renderer->render($response, 'detail.phtml', $args);
 });
